f7fdf81d4b3bfcb93fe8f9d97372b524e23da577
Jessie Conley <jconley2881@gmail.com>
2020-03-18 11:21:09 -0500

    user page started, break point

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -5,13 +5,31 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
-const UserPage = (props) => (
+const UserPage = props => (
   <div>
-    <h1 id="welcome">
-      Welcome, { props.user.username }!
-    </h1>
-    <p>Your ID is: {props.user.id}</p>
-    <LogOutButton className="log-in" />
+    <h1 id="welcome">Welcome, {props.user.name}!</h1>
+    {/* <p>Your ID is: {props.user.id}</p> */}
+    {/* <LogOutButton className="log-in" /> */}
+    <center>
+      <p>Who's responsibility chart are we workin with today?</p>
+      <button>Child Name 1</button>
+      <br></br>
+      <br></br>
+      <button>Child Name 2</button>
+      <br></br>
+    </center>
+    <br></br>
+    <center>
+      <button
+        type="button"
+        // className="link-button"
+        onClick={() => {
+          this.props.dispatch({ type: "SET_TO_REGISTER_MODE" });
+        }}
+      >
+        Add Child
+      </button>
+    </center>
   </div>
 );
 
